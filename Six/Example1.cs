0124f920317b63f4298fdef66dcd12eb8090c557
Elena <ARSSNEK@yandex.ru>
2020-08-17 10:50:15 +0300

    SixHomeWork

---
+++ Six/Example1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HomeWork
+{
+    public delegate double Fun(double x, double a);
+    class Program
+    {
+        public static void Table(Fun F, double x, double a, double b)
+        {
+            Console.WriteLine("----- X ----- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} |", x, F(x, a));
+                x += 1;
+            }
+            Console.WriteLine("---------------------");
+        }
+        public static double MyFunc(double x, double a)
+        {
+            return a * x * x;
+        }
+        public static double Sinus(double x, double a)
+        {
+            return a * Math.Sin(x);
+        }
+        private static void Main()
+        {
+            Console.WriteLine("Таблица функции Myfunc : ");
+            Table(MyFunc, -2, 3, 2);
+            Console.WriteLine("Таблица функции Sinus : ");
+            Table(Sinus, -2, 3, 2);
+
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Six/Example3.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Example3
+{
+     class Program
+    {
+        private static void Main(string[] args)
+        {
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            int l = 0;
+
+
+            int[] Kr = new int[7];
+
+            StreamReader sr = new StreamReader("students_1.txt");
+            string[] asds;
+            ArrayList list = new ArrayList();
+            ArrayList list2 = new ArrayList();
+            //StreamWriter srwr = new StreamWriter("students_1.txt");
+            while (!sr.EndOfStream)
+            {
+                try
+                {
+                    string[] s = sr.ReadLine().Split(';');
+
+                    list.Add(s[3] + " лет  - " + s[0] + " " + s[1]);
+                    list2.Add(" " + s[2] + "      " + s[3] + " лет - " + s[0] + " " + s[1]);
+
+
+                    if (int.Parse(s[2]) == 5 || int.Parse(s[2]) == 6)
+                    {
+                        c++;
+                    }
+                    if (int.Parse(s[3]) >= 18 && int.Parse(s[3]) <= 22)
+                    {
+                        b++;
+                        for (int a = 1; a <= 6; a++)
+                        {
+                            if (int.Parse(s[2]) == a)
+                            {
+                                Kr[a]++;
+                            }
+                        }
+                    }
+                    d++;
+                    l++;
+
+                }
+                catch
+                {
+                }
+            }
+            list.Sort();
+            list2.Sort();
+
+            
+
+
+
+
+
+            sr.Close();
+
+
+
+            Console.WriteLine($"Студентов на 5-6 курсах : {c} ");
+
+            
+
+
+
+
+            Console.WriteLine($"\nСтудентов в возрасте 18 - 22 лет - {b}");
+            for (int a = 0; a < Kr.Length; a++)
+            {
+                if (Kr[a] > 0)
+                {
+                    Console.WriteLine($"На {a} курсе учатся - {Kr[a]}");
+                }
+            }
+            Console.WriteLine();
+            foreach(var f in list)
+            {
+                Console.WriteLine(f);
+            }
+            Console.WriteLine($"\nКУРС   ВОЗРАСТ     ФИ    ");
+            foreach (var fa in list2)
+            {
+                
+                Console.WriteLine(fa);
+            }
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
